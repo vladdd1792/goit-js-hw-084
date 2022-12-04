e1a777f417d3209e952300d22e05f444617e4131
VladVeselov <vlaq1997@gmail.com>
2022-12-04 15:09:26 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+player.setCurrentTime(localStorage.getItem ('setCurrentTime(|)')||0);
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+
+function onPlay({ seconds }) {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  }
