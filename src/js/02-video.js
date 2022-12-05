c0fcc575a460963623ea05e35b756be7d74566f1
VladVeselov <vlaq1997@gmail.com>
2022-12-05 12:26:29 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,9 +4,8 @@ const iframe = document.querySelector('iframe');
 
 const player = new Player(iframe);
 
-player.on('timeupdate', throttle(onPlay, 1000));
-
 player.setCurrentTime(localStorage.getItem ('videoplayer-current-time(|)')||0);
+player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay({ seconds }) {
     localStorage.setItem('videoplayer-current-time', seconds);
