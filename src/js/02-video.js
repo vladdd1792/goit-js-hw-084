665c9ffc7ccef2109e40efec83e8c1e7b71a8c06
VladVeselov <vlaq1997@gmail.com>
2022-12-05 12:29:43 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,12 +4,13 @@ const iframe = document.querySelector('iframe');
 
 const player = new Player(iframe);
 
-player.setCurrentTime(localStorage.getItem ('videoplayer-current-time(|)')||0);
 player.on('timeupdate', throttle(onPlay, 1000));
+
 
 function onPlay({ seconds }) {
     localStorage.setItem('videoplayer-current-time', seconds);
-  }
+    
+    player.setCurrentTime(localStorage.getItem ('videoplayer-current-time(|)')||0); }
 
   /*import Player from '@vimeo/player';
   import throttle from 'lodash.throttle';
